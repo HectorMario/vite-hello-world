96dcfd99f5048ec5cab73fefd4881fb8a4917312
HectorMario <hector.mario.peraz@gmail.com>
2023-04-18 10:53:25 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import tittle from "./components/tittle.vue";
+import jumbo from "./components/jumbo.vue";
+
+export default {
+  components: {
+    tittle,
+    jumbo
+  }
+}
+</script>
+
+<template>
+
+<tittle/>
+<jumbo/>
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/tittle.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'AppTitle',
+    
+}
+</script>
+
+
+<template>
+    <h1>Hello word</h1>
+</template>
+
+
+<style>
+h1 {
+    color: rgb(139, 55, 55);
+    text-align: center;
+    
+}
+</style>
